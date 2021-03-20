165a4c15c97c3e76d8ac6543a9431b7d4e70e8d5
aserdnad <andreasgamardo@gmail.com>
2021-03-20 21:22:30 +0100

    ya se cambia la frase

--- src/app.js
+++ src/app.js
@@ -5,7 +5,25 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch"
+];
+let excusa = [];
+let cambio = document.querySelector("#excusa");
+
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  for (let i = 0; i <= 3; i++) {
+    excusa.push(Math.floor(Math.random() * 4));
+  }
+  cambio.innerHTML = `${who[excusa[0]]} ${action[excusa[1]]} ${
+    what[excusa[2]]
+  } ${when[excusa[3]]}`;
 };
